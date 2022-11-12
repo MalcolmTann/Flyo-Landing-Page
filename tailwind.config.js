1c31a37f8f8f7da4f6709cb812515024f9118858
MacolmTann <malcolmtan20@gmail.com>
2022-11-12 15:15:44 -0800

    init custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./public/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        intro: '#1c2431',
+        main: '#181f2a',
+        test: '#202a3c',
+        footer: '#0b1523',
+        white: '#fff',
+        gradient: 'linear-gradient(90deg, rgba(101,226,217,1) 0%, rgba(51,158,204,1) 100%)',
+        error: '#ff4242',
+      },
+    },
   },
   plugins: [],
 }
+
+
