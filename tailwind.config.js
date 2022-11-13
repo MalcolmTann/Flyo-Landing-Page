abbdbeb65e6cb887d39302040d47840301c93ae7
MacolmTann <malcolmtan20@gmail.com>
2022-11-12 16:05:24 -0800

    complete header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ['./public/*.html'],
   theme: {
     extend: {
+      fontFamily: {
+        openSans: "'Open Sans', sans-serif",
+        railway: "'Raleway', sans-serif",
+      },
+
       colors: {
         intro: '#1c2431',
         main: '#181f2a',
@@ -12,6 +17,7 @@ module.exports = {
         gradient: 'linear-gradient(90deg, rgba(101,226,217,1) 0%, rgba(51,158,204,1) 100%)',
         error: '#ff4242',
       },
+
     },
   },
   plugins: [],
