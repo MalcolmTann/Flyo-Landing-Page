c744773f7c4a366c7ffc3e1c42f95b13590743f3
MacolmTann <malcolmtan20@gmail.com>
2022-11-18 23:36:14 -0800

    Adjusted landing image responsiveness

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,31 @@
-
+// Change Background image at 768 viewport
 function changeBg() {
 
+    // if the viewport matches or is greater than 768px
     if(matchMedia('(min-width: 768px)').matches) {
-
+        // use the desktop version of the background
         document.getElementById('intro-bg-js').style.backgroundImage = "url('images/bg-curvy-desktop.svg')";
 
     } else {
-
+        // if the viewport is less than 768px, use the mobile backgound image instead
         document.getElementById('intro-bg-js').style.backgroundImage = "url('images/bg-curvy-mobile.svg')";
     }
 }
 
+// changeContent function called to instantiate changeBg()
 function ChangeContent() {
     changeBg();
 
+    // add an event listener so the function knows when to trigger - change the bg
     matchMedia('(min-width: 768px)').addEventListener('change', () => {
         changeBg();
     });
 }
-
+// call the function
 ChangeContent();
 
+
+// Email Validation
 function ValidateEmail() {
     
     var form = document.getElementById('form');
